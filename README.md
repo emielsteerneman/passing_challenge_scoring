86647507c391fa0c333bd9f7507a144a661422a1
Emiel Steerneman <emiel.steerneman@rabobank.nl>
2025-02-16 21:37:18 +0100

    Updated animation and README

--- README.md
+++ README.md
@@ -1,12 +1,53 @@
-# Pass challenge scoring system
+## The current situation. More passes = better ranking
+In the 2024 tournament, the ranking of the passing challenge was calculated by counting up the total number of passes each team had made. The team with the most passes received 1st place, the team with the second most passes 2nd place, and so on. 
 
-The number of passes made per team does not accurately reflect a team its performance, since one team could face either two very strong or very weak opponents. An example of this can be found in the code. The total number of passes made per team should therefore not be used as a final ranking. This scoring system attempts to solve the problem of last years passing challenge. 
+## The issue with this system
+Let's pick two strong teams, ZJUNLict and TIGERs Mannheim. What if these are your two opponents for the passing challenge? It's definitely going to be difficult. Yet, your team persists and you make 2 passes against ZJUNLict (they make 5), and 4 passes against TIGERs (they make 1). Your total number of passes is now 2 + 4 = 6. That's not a lot, but what do you expect against two top-tier teams. 
 
-# The Big Picture
+Now, let's also take two low-tier Div B teams, and another team, RoboTeam Twente. RoboTeam Twente plays quite well against these two teams, making 4 + 7 = 11 passes. RoboTeam Twente made more passes than your team, so they have a higher ranking. But that doesn't seem really fair does it? You had to play against two top-tier Div-A teams, and RoboTeam Twente against two low-tier Div B teams! 
 
-Imagine all teams placed in a circle. Whenever two teams play a match, we connect them with an imaginary “spring.” The more one team outscored the other, the more that spring is stretched or compressed. The code then “relaxes” these springs until everyone settles into a position that reflects how strongly they beat or lost to each other over the whole tournament.
+Just looking at the total number of passes made doesn't work. So, how do we wrong this right? We somehow have to take into account that one team might just have two very strong opponents, while another might have two weak opponents. Having even more matches to have your team play against RoboTeam Twente isn't an option, so ..?
 
-# Step-by-Step Explanation
+## An exaggerated example
+If you pass as well as another team (meaning, an almost equal number of passes in a match), it makes sense that you and the other team have around the same ranking. If you destroy another team, your team should have a much higher ranking than that other team. Therefore, let's start with an exaggerated example tournament.
+
+Team 1 | Passes | Team 2
+---: | :---: | :--- 
+ZJUNLict | 5 - 2 | ER-Force
+ER-Force | 4 - 1 | TIGERs
+TIGERs | 45 - 3 | KIKS
+KIKS | 8 - 11 | RoboDragons
+RoboDragons | 12 - 50 | ZJUNLict
+
+This brings the total number of passes for each team to the following:
+
+Team | Passes | Ranking
+---: | --- | ---
+ZJUNLict    | 55 | 1st
+ER-Force    |  6 | 5th
+TIGERs      | 46 | 2nd
+KIKS        | 11 | 4th
+RoboDragons | 23 | 3rd
+ 
+This ranking doesn't make much sense. ER-Force played so well against ZJUNLict and even beat TIGERs. Yet, it's placed last? The problem here is that ER-Force had two strong opponents, while ZJUNLict and TIGERs both also had one relative weak opponent.  
+
+## Let's try ranking by relative score
+Let's start by putting ER-Force at a score of 0. This score is relative to the scores of other teams, so 0 doesn't say anything yet. ER-Force had 3 more passes in it's match against TIGERs, so let's place TIGER's at -3. KIKS, because of its -42 pass difference against TIGERs will move to -3 - 42 = -45. RoboDragons will move to -45 + 3 = -42. ZJUNLict will move to -42 + 38 = -4. Now let's look  at the score again.
+
+Team | Score | Ranking
+---: | --- | ---
+ZJUNLict    | -4 | 3rd
+ER-Force    |  0 | 1st
+TIGERs      | -3 | 2nd
+KIKS        | -45 | 5th
+RoboDragons | -42 | 4th
+
+This makes a bit more sense. ER-Force ends up above TIGERs, which it won against. It also placed above ZJUNLict, against which it lost. That still feels a bit weird.. **But wait**. If we continue the scoring system, given that ZJUNLict had 3 passes more against ER-Force, ER-Force should end up at the score -4 - 3 = -7 !? But ER-Force already received the score of 0 all the way at the beginning.. What now? Does ER-Force receive a score of 0 or -7? If we give ER-Force the score of 0, it stays on the 1st place. If we give it the score of -7, it receives 3rd place. But then, at -7, shouldn't TIGERs also move down again to -7 - 3 = -10? Maybe we should give ER-Force a score of -3.5, in the middle. But then again, TIGERs would have to move down to -6.5? Maybe TIGERs its score should then be averaged as well? And because TIGERs moves down, KIKS moves down again as well. As you can see, this is an infinite circle, where we keep pushing the score of teams up and down. Fortunately, there is a solution to this seemingly infinite process. 
+
+
+
+
+
 
 ### Start with each team at a score of 0
 
--- main.py
+++ main.py
@@ -14,44 +14,44 @@ RTT = "RoboTeam Twente"
 
 
 """ EXAMPLE TOURNAMENT """
-TEAMS_CYCLE = [ERFORCE, LUHBOTS, KIKS, ROBODRAGONS, ZJUNLICT]
+TEAMS_CYCLE = [ERFORCE, TIGERS, KIKS, ROBODRAGONS, ZJUNLICT]
 
 MATCHES = [
-    [ZJUNLICT, ERFORCE, 3, 2],
-    [ERFORCE, LUHBOTS, 2, 1],
-    [LUHBOTS, KIKS, 45, 3],
-    [KIKS, ROBODRAGONS, 8, 10],
+    [ZJUNLICT, ERFORCE, 5, 2],
+    [ERFORCE, TIGERS, 4, 1],
+    [TIGERS, KIKS, 45, 3],
+    [KIKS, ROBODRAGONS, 8, 11],
     [ROBODRAGONS, ZJUNLICT, 12, 50]
 ]
 
-# ZJUNLict    - 53 passes total
-# ER-Force    -  5 passes total
-# LUHBots     - 46 passes total
-# KIKS        - 13 passes total
-# RoboDragons - 22 passes total
+# ZJUNLict    - 55 passes total
+# ER-Force    -  6 passes total
+# TIGERS     - 46 passes total
+# KIKS        - 11 passes total
+# RoboDragons - 23 passes total
 
 ## Ranking based on passes:
-# ZJUNLict > LUHbots > RoboDragons > KIKS > ER-Force
-# This makes no sense since ER-Force won against LUHBots and basically tied with ZJUNLict
+# ZJUNLict > TIGERS > RoboDragons > KIKS > ER-Force
+# This makes no sense since ER-Force won against TIGERS and basically tied with ZJUNLict
 
 
 
 
 
-""" 2024 TOURNAMENT """
-TEAMS_CYCLE = [ERFORCE, LUHBOTS, KIKS, RTT, TIGERS, ROBODRAGONS, ZJUNLICT]
+# """ 2024 TOURNAMENT """
+# TEAMS_CYCLE = [ERFORCE, LUHBOTS, KIKS, RTT, TIGERS, ROBODRAGONS, ZJUNLICT]
 
-# Match schedule here https://docs.google.com/spreadsheets/d/1wFbCYw-gfPdE7WykX31WSTPAvU3SWHysxapoCXWekbA/edit?gid=1087321196#gid=1087321196
-# Score sheet    here https://docs.google.com/spreadsheets/d/1KiqHZhVfNrZQpCc0nftIHPvuw9C-P_diZZ-TQojkGdM/edit?gid=1863286970#gid=1863286970
-MATCHES = [
-    [ERFORCE, LUHBOTS, 9, 4],
-    [ZJUNLICT, ROBODRAGONS, 67, 0],
-    [RTT, TIGERS, 5, 52],
-    [KIKS, RTT, 5, 17],
-    [TIGERS, ROBODRAGONS, 65, 1],
-    [ERFORCE, ZJUNLICT, 5, 11],
-    [KIKS, LUHBOTS, 7, 10]
-]
+# # Match schedule here https://docs.google.com/spreadsheets/d/1wFbCYw-gfPdE7WykX31WSTPAvU3SWHysxapoCXWekbA/edit?gid=1087321196#gid=1087321196
+# # Score sheet    here https://docs.google.com/spreadsheets/d/1KiqHZhVfNrZQpCc0nftIHPvuw9C-P_diZZ-TQojkGdM/edit?gid=1863286970#gid=1863286970
+# MATCHES = [
+#     [ERFORCE, LUHBOTS, 9, 4],
+#     [ZJUNLICT, ROBODRAGONS, 67, 0],
+#     [RTT, TIGERS, 5, 52],
+#     [KIKS, RTT, 5, 17],
+#     [TIGERS, ROBODRAGONS, 65, 1],
+#     [ERFORCE, ZJUNLICT, 5, 11],
+#     [KIKS, LUHBOTS, 7, 10]
+# ]
 
 # ER-Force 9 - 4 LUHBots 10 - 7 KIKS 5 - 17 RTT 5 - 52 TIGERs Mannheim 65 - 1 RoboDragons 0 - 67 ZJUNlict 11 - 5 ER-Force
 
@@ -66,47 +66,75 @@ MATCHES = [
 
 
 
-
+def show(image, timeout=0):
+    cv2.imshow("image", image)
+    if cv2.waitKey(timeout) == ord('q'):
+        exit()
 
 def render(teams, springs, values, timeout=0):
-    GRID_STEP = 3
-    
-    def show(image, timeout=0):
-        cv2.imshow("image", image)
-        if cv2.waitKey(timeout) == ord('q'):
-            exit()
+    GRID_STEP = 4
+    WHITE = (255, 255, 255)
+    BACKGROUND = (10, 10, 10)
+    FONT = cv2.FONT_HERSHEY_SIMPLEX
 
     image = np.zeros((800, 1200, 3), np.uint8)
-    YCENTER = 4 * image.shape[0] // 5
+    image.fill(BACKGROUND[0])
+    YCENTER = int(image.shape[0] * 0.8)
     XSTEP = 1000 // len(teams)
+
+    # Determine minimum and maximum values. Check if frame needs to be resized
+    upper_y, lower_y = YCENTER - max(values)*GRID_STEP, YCENTER - min(values)*GRID_STEP
+    if upper_y < 0 or 1200 < lower_y:
+        print("NOT GOING TO FIT!")
+        # Modify grid step to fit the values
+        GRID_STEP = min(6, 0.8*(YCENTER)/max(values), 0.8*(1200-YCENTER)/min(values))
+    print("Upper Y:", upper_y, "Lower Y:", lower_y, "Grid step:", GRID_STEP)
+
+    to_x = lambda x: 200 + int(x*XSTEP)
+    to_y = lambda y: YCENTER - int(y*GRID_STEP)
+
 
     ### Draw grid
     for i in range(0, 140, 10):
         # Vertical
-        cv2.line(image, (200*i, 0), (200*i, 1000), (50, 50, 50), 1)
+        # cv2.line(image, (20*i, 0), (20*i, 1000), (20, 20, 20), 1)
         # Horizontal
-        cv2.line(image, (0, YCENTER - i * GRID_STEP), (1200, YCENTER - i * GRID_STEP), (50, 50, 50), 1)
-        cv2.line(image, (0, YCENTER + i * GRID_STEP), (1200, YCENTER + i * GRID_STEP), (50, 50, 50), 1)
-    cv2.line(image, (0, YCENTER), (1200, YCENTER), (255, 255, 255), 1)
-    
+        cv2.line(image, (0, to_y(i)), (1200, to_y(i)), (50, 50, 50), 1)
+        cv2.line(image, (0, to_y(-i)), (1200, to_y(-i)), (50, 50, 50), 1)
+    cv2.line(image, (0, to_y(0)), (1200, to_y(0)), WHITE, 1)
+
     ### Draw teams
     get_color = lambda i: (255 - i*50, 0, i*50)
 
-    for i, (team, spring, value) in enumerate(zip(teams, springs, values)):
-        x, y = 200+i*XSTEP, YCENTER-int(value*GRID_STEP)
-        x2, y2 = 200+(i+1)*XSTEP, YCENTER-int(values[(i+1)%len(teams)]*GRID_STEP)
+    values_shift_one = values[1:] + [values[0]]
+    for i, (team, spring, value, next_value) in enumerate(zip(teams, springs, values, values_shift_one)):
+        x, y = to_x(i), to_y(value)# YCENTER-int(value*GRID_STEP)
+        x2, y2 = to_x(i+1), to_y(next_value) #YCENTER-int(values[(i+1)%len(teams)]*GRID_STEP)
 
+        # Determine ranking of team
         ranking = sum([ v >= value for v in values ])
+        # Draw circle, ranking within the circle, and name at the top
         cv2.circle(image, (x, y), 20, get_color(i), -1, cv2.LINE_AA)
-        cv2.putText(image, f"{ranking}", (x-5, y+5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-        cv2.putText(image, f"{team}: {value:.2f}", (x-50, 50+(i%2)*25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image, f"{ranking}", (x-5, y+5), FONT, 0.5, WHITE, 1, cv2.LINE_AA)
+        cv2.putText(image, f"{team}: {value:.2f}", (x-50, 50+(i%2)*25), FONT, 0.5, WHITE, 1, cv2.LINE_AA)
 
         
         string = f"^ {spring} v" if spring > 0 else f"v {abs(spring)} ^"
         if spring == 0: string = f"= {spring} ="
 
-        # cv2.rectangle(image, (x + XSTEP//2 - 30, (y + y2)//2 - 15), (x + XSTEP//2 + 30, (y + y2)//2 + 10), (0, 0, 0), -1)
-        cv2.putText(image, string, (x + XSTEP//2 - 20, (y + y2)//2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        # Draw a fancy spring (zigzagging lines)
+        spring_x = x + XSTEP//2
+        dy = abs(y - y2)
+        spring_ = abs(spring)
+        for i in range(spring_):
+            wire_x1 = 5 if i % 2 == 0 else -5
+            wire_y1 = min(y, y2) + int(i * (dy/spring_))
+            wire_x2 = 5 if i % 2 == 1 else -5
+            wire_y2 = min(y, y2) + int((i+1) * (dy/spring_))
+            cv2.line(image, (spring_x + wire_x1, wire_y1), (spring_x + wire_x2, wire_y2), (255, 255, 0), 1, cv2.LINE_AA)
+
+        cv2.putText(image, string, (x + XSTEP//2 - 20, (y + y2)//2), FONT, 0.6, BACKGROUND, 4, cv2.LINE_AA)
+        cv2.putText(image, string, (x + XSTEP//2 - 20, (y + y2)//2), FONT, 0.6, WHITE, 1, cv2.LINE_AA)
 
     show(image, timeout)
 
@@ -148,7 +176,8 @@ if __name__ == "__main__":
     passes_per_team = { team: get_total_passes(MATCHES, team) for team in TEAMS_CYCLE }
 
     # Create initial state
-    initial = [ 0. for team in TEAMS_CYCLE ]
+    # initial = [ 0. for team in TEAMS_CYCLE ]
+    initial = [ passes_per_team[team] for team in TEAMS_CYCLE ]
     springs = []
     # For each team pair that played against each other, calculate the difference in passes
     for team1, team2 in zip(TEAMS_CYCLE, TEAMS_CYCLE[1:] + [TEAMS_CYCLE[0]]):
@@ -166,7 +195,7 @@ if __name__ == "__main__":
 
     for i in range(1000):
         print(f"Step {i:>5} ", end="\r")
-        render(TEAMS_CYCLE, springs, current, timeout=33)
+        render(TEAMS_CYCLE, springs, current, timeout=200)
     
         current = solve_spring_system(current, springs, step=0.01 + 0.01*i)
     
@@ -181,4 +210,4 @@ if __name__ == "__main__":
         print(f"{team:<16} | {score:.2f}")
 
     # System converged. Render the final state
-    render(TEAMS_CYCLE, springs, current, timeout=0)
+    render(TEAMS_CYCLE, springs, current, 0)
